6dad7b20be2859b93b258e72b65e1bf40ae2379a
Ruderud <jungkh05@naver.com>
2021-10-04 11:26:46 +0900

    404error page 접근시, 일정시간 후 로그인페이지로 이동

--- src/pages/Error/Error404.tsx
+++ src/pages/Error/Error404.tsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 interface ErrorPage {
   url: string;
 }
 
 const Error404: React.FC<ErrorPage> = () => {
+  setTimeout(() => {
+    window.location.href = "http://localhost:3000/";
+  }, 3000);
+
   return (
     <>
       <ul>Error 404</ul>
       <ul>잘못된 페이지 접근입니다.</ul>
+      <ul>잠시 후 로그인 페이지로 이동합니다.</ul>
     </>
   );
 };
