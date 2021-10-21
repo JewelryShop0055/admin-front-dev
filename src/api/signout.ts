6f9a5dcba61122dca1ce1d1abe7ca6e9053f38d2
Ruderud <jungkh05@naver.com>
2021-10-21 17:58:16 +0900

    signout api 수정

--- src/api/signout.ts
+++ src/api/signout.ts
@@ -3,16 +3,12 @@ import axios from "axios";
 export async function signOut(accessToken: string) {
   const URL = `${process.env.REACT_APP_SERVER_BASE_URL}/admin/account/signout`;
   const bodyProps = `access_token=${accessToken}`;
-  try {
-    const response = await axios.delete(URL, {
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-      data: bodyProps,
-    });
-    return Object(response.data);
-  } catch (e) {
-    console.log("signoutAPI 요청에서 문제발생: ", e);
-    return String(e);
-  }
+
+  const response = await axios.delete(URL, {
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    data: bodyProps,
+  });
+  return response.data;
 }
