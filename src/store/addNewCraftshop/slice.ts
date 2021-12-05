498855d438ddf9e911a7a46126daf6aa43d7f2f3
Ruderud <jungkh05@naver.com>
2021-12-05 21:48:05 +0900

    공방추가 slice 작성

---
+++ src/store/addNewCraftshop/slice.ts
@@ -0,0 +1,45 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { AddNewCraftshopParams } from "../../types";
+
+interface AddNewCraftshopState extends AddNewCraftshopParams {
+  isLoadingAddCategory: boolean;
+}
+
+const initialState: AddNewCraftshopState = {
+  name: "",
+  postCode: "",
+  address: "",
+  detailAddress: "",
+  phone: "",
+  isLoadingAddCategory: false,
+};
+
+export const addNewCraftshopSlice = createSlice({
+  name: "addCraftshop",
+  initialState,
+  reducers: {
+    addNewCraftshopPending: (
+      state,
+      action: PayloadAction<AddNewCraftshopState>
+    ) => {
+      state.isLoadingAddCategory = true;
+    },
+    addNewCraftshopFullfilled: (
+      state,
+      action: PayloadAction<AddNewCraftshopParams>
+    ) => {
+      state.name = action.payload.name;
+      state.postCode = action.payload.postCode;
+      state.address = action.payload.address;
+      state.detailAddress = action.payload.detailAddress;
+      state.phone = action.payload.phone;
+      state.isLoadingAddCategory = false;
+    },
+    addNewCraftshopRejected: (state) => {
+      state.isLoadingAddCategory = false;
+    },
+  },
+});
+
+export const { actions } = addNewCraftshopSlice;
+export default addNewCraftshopSlice.reducer;
